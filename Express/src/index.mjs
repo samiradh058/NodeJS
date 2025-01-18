489b96ec474570d958ce6f147bddf0bbecf3b9c0
samiradh058 <adhikarisamir68@gmail.com>
2025-01-18 22:58:29 +0545

    Cookies

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("SAM"));
 app.use(routes);
 
 const loginMiddleware = (req, res, next) => {
@@ -19,16 +21,10 @@ app.use(loginMiddleware, (req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("Base URL");
-    next();
-  },
-  (req, res, next) => {
-    res.status(201).send("Hello, World!");
-  }
-);
+app.get("/", (req, res, next) => {
+  res.cookie("hello", "world", { maxAge: 60000, signed: true });
+  res.status(201).send("Hello, World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- Express/src/routes/products.mjs
+++ Express/src/routes/products.mjs
@@ -3,10 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res, next) => {
-  res.send([
-    { name: "product1", price: 100 },
-    { name: "product2", price: 150 },
-  ]);
+  console.log(req.cookies);
+  console.log(req.signedCookies.hello);
+  if (req.signedCookies.hello && req.signedCookies.hello === "world") {
+    return res.send([
+      { name: "product1", price: 100 },
+      { name: "product2", price: 150 },
+    ]);
+  }
+  return res.send({ msg: "You need correct cookie" });
 });
 
 export default router;
