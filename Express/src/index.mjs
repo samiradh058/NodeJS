be82d3041305e45ea85571c0f3d4a4de4cd3bd84
samiradh058 <adhikarisamir68@gmail.com>
2025-01-20 11:41:03 +0545

    session

--- Express/src/index.mjs
+++ Express/src/index.mjs
@@ -3,6 +3,8 @@ import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 
+import { users } from "./utils/constants.mjs";
+
 const app = express();
 
 app.use(express.json());
@@ -38,6 +40,47 @@ app.get("/", (req, res, next) => {
   req.session.visited = true;
   res.cookie("hello", "world", { maxAge: 60000, signed: true });
   res.status(201).send("Hello, World!");
+});
+
+app.post("/api/auth", (req, res, next) => {
+  const {
+    body: { name, password },
+  } = req;
+  const findUser = users.find((user) => user.name === name);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "Bad credentials" });
+
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res, next) => {
+  req.sessionStore.get(req.session.id, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not authenticated" });
+});
+
+app.post("/api/cart", (req, res, next) => {
+  if (!req.session.user) return res.sendStatus(401);
+
+  const { body: item } = req;
+
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res, next) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.status(200).send(req.session.cart ?? []);
 });
 
 app.listen(PORT, () => {
--- Express/src/utils/constants.mjs
+++ Express/src/utils/constants.mjs
@@ -1,6 +1,6 @@
 export const users = [
-  { id: 1, name: "ram", age: 42 },
-  { id: 2, name: "samir", age: 20 },
-  { id: 3, name: "ali", age: 25 },
-  { id: 4, name: "sangam", age: 15 },
+  { id: 1, name: "ram", age: 42, password: "hello1" },
+  { id: 2, name: "samir", age: 20, password: "hello2" },
+  { id: 3, name: "ali", age: 25, password: "hello3" },
+  { id: 4, name: "sangam", age: 15, password: "hello4" },
 ];
